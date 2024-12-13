0f7a186b273a427a6d40bfe9bde53f93e80b0f56
mhemeryck <martijn.hemeryck@gmail.com>
2024-12-13 16:05:29 +0000

    setup for isabel managed device

---
+++ nixos-tinkerbell/configuration.nix
@@ -0,0 +1,53 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+# NixOS-WSL specific options are documented on the NixOS-WSL repository:
+# https://github.com/nix-community/NixOS-WSL
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    # include NixOS-WSL modules
+    <nixos-wsl/modules>
+  ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  wsl.enable = true;
+  wsl.defaultUser = "mhemeryck";
+
+  security.pki.certificateFiles = [
+    ./certs/cert_ISA_GP_Intermediate.pem
+    ./certs/cert_ISA_GP_Root.pem
+    ./certs/cert_ISA_SSL-FWD-Trust.pem
+  ];
+
+  users.users.mhemeryck = {
+    isNormalUser = true;
+    description = "Martijn Hemeryck";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs;
+      [
+        #  thunderbird
+      ];
+    shell = pkgs.nushell;
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+  environment.systemPackages = with pkgs; [
+    bat
+    git
+    home-manager
+    helix
+    openssh
+  ];
+}
