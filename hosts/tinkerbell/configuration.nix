d262b62c74aaaa53ac773140896e7b8bc09522e8
mhemeryck <martijn.hemeryck@gmail.com>
2024-12-14 21:49:03 +0000

    fix hostname stuff
    apparenlty, the config name needs to match the hostname.

--- hosts/tinkerbell/configuration.nix
+++ hosts/tinkerbell/configuration.nix
@@ -29,6 +29,8 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   # system.stateVersion = "24.05"; # Did you read the comment?
 
+  networking.hostName = "tinkerbell";
+
   environment.systemPackages = with pkgs; [
     bat
     git
