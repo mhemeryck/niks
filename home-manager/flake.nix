6ebd13fc2244ffc22004df541e50fccf713f5353
mhemeryck <martijn.hemeryck@gmail.com>
2025-02-12 16:25:42 +0000

    Alternate version with an override

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -21,32 +21,7 @@
 
           # Specify your home configuration modules here, for example,
           # the path to your home.nix.
-          modules = [
-            ./home.nix
-            {
-              # Override google-cloud-sql-proxy version
-              nixpkgs.overlays = [
-                (self: super: {
-                  google-cloud-sql-proxy =
-                    super.google-cloud-sql-proxy.overrideAttrs (old: {
-                      version = "2.12.0"; # Specify the desired version
-                      src = super.fetchFromGitHub {
-                        owner = "GoogleCloudPlatform";
-                        repo = "cloudsql-proxy";
-                        rev = "v2.12.0";
-                        hash =
-                          "sha256-nEbrNRrEXXvLYi1vIvukUaq+WQn2HlonaaMn57yIA3I=";
-                      };
-
-                      vendorHash =
-                        "sha256-EI2PDVdS9JB8ACkRTsfCBLz4JEmHQ6hApFSSfSvD/cQ=";
-                      subPackages = [ "." ];
-                      checkFlags = [ "-short" ];
-                    });
-                })
-              ];
-            }
-          ];
+          modules = [ ./home.nix ];
 
           # Optionally use extraSpecialArgs
           # to pass through arguments to home.nix
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,6 +1,21 @@
 { pkgs, ... }:
 
-rec {
+let
+  # Override for cloud-sql-proxy
+  google-cloud-sql-proxy-v12 = pkgs.google-cloud-sql-proxy.overrideAttrs
+    (finalAttrs: previousAttrs: rec {
+      version = "2.12.0";
+      src = pkgs.fetchFromGitHub {
+        owner = "GoogleCloudPlatform";
+        repo = "cloudsql-proxy";
+        rev = "v${version}";
+        hash = "sha256-nEbrNRrEXXvLYi1vIvukUaq+WQn2HlonaaMn57yIA3I=";
+      };
+      vendorHash = "sha256-EI2PDVdS9JB8ACkRTsfCBLz4JEmHQ6hApFSSfSvD/cQ=";
+      subPackages = [ "." ];
+      checkFlags = [ "-short" ];
+    });
+in rec {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "mhemeryck";
@@ -24,7 +39,7 @@ rec {
     cargo
     direnv
     dprint
-    google-cloud-sql-proxy
+    google-cloud-sql-proxy-v12
     (google-cloud-sdk.withExtraComponents
       [ google-cloud-sdk.components.gke-gcloud-auth-plugin ])
     gh
