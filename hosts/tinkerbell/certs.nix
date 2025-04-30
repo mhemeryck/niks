5126f87657ecd528bef046fe1f2a57bd530d7f92
mhemeryck <martijn.hemeryck@gmail.com>
2025-04-30 07:01:41 +0000

    Replace all wk certs by single one
    They were all identical

--- hosts/tinkerbell/certs.nix
+++ hosts/tinkerbell/certs.nix
@@ -4,8 +4,6 @@
     ./certs/cert_ISA_GP_Intermediate.pem
     ./certs/cert_ISA_GP_Root.pem
     ./certs/cert_ISA_SSL-FWD-Trust.pem
-    ./certs/wk1.cer
-    ./certs/wk2.cer
-    ./certs/wk3.cer
+    ./certs/wk.cer
   ];
 }
