87f74147cae54fae6f28ba44b01ef8ed06edf399
mhemeryck <martijn.hemeryck@gmail.com>
2025-06-30 07:17:16 +0000

    use fixes for oh-my-posh in home manager

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -14,32 +14,7 @@
     { nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          (final: prev: {
-            oh-my-posh = final.stdenv.mkDerivation {
-              pname = "oh-my-posh";
-              version = "v25.23.3";
-
-              src = final.fetchurl {
-                url = "https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/v25.23.3/posh-linux-amd64";
-                sha256 = "sha256-q7MgTdM6oKZv2gpaYv+j3K4pNW83Cz99rb5+GRjj6Rw=";
-              };
-
-              phases = [ "installPhase" ];
-
-              installPhase = ''
-                mkdir -p $out/bin
-                cp $src $out/bin/oh-my-posh
-                chmod +x $out/bin/oh-my-posh
-              '';
-
-              meta.mainProgram = "oh-my-posh";
-            };
-          })
-        ];
-      };
+      pkgs = nixpkgs.legacyPackages.${system};
     in
     {
       homeConfigurations."mhemeryck" = home-manager.lib.homeManagerConfiguration {
